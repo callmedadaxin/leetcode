908d4b1194c19e2066825de8eac298666a5dca8d
callmedadaxin <554091944@qq.com>
2017-03-10 17:56:06 +0800

    complete roman to integer

---
+++ src/13.roman-to-integer.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+  var dictionary = {
+      I: 1,
+      V: 5,
+      X: 10,
+      L: 50,
+      C: 100,
+      D: 500,
+      M: 1000
+    },
+    i = 1,
+    result = val = dictionary[s[0]];
+
+  while (i < s.length) {
+    var newVal = dictionary[s[i]];
+
+    if (newVal <= val) {
+      result += newVal;
+      console.log('+' + newVal)
+    } else {
+      result += newVal - 2 * val;
+      console.log('+' + newVal + '-' + 2 * val)
+    }
+
+    i++;
+    val = newVal;
+  }
+
+  return result;
+};
+
+console.log(romanToInt("MCMXCVI"))
